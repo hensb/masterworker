b83e67c9a60c56e9f43a977c37676b7455b97a0f
Hendrik Schoeneberg <sb.hendrik@gmail.com>
2013-12-16 08:11:15 +0100

    akka 2.2.3 actor lookup implemented

--- src/main/scala/de/uniwue/info2/masterworker/Worker.scala
+++ src/main/scala/de/uniwue/info2/masterworker/Worker.scala
@@ -17,19 +17,41 @@ import de.uniwue.info2.masterworker.Master.WorkerRequestsWork
 import de.uniwue.info2.masterworker.Worker.DoneWorking
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
+import akka.actor.Identify
+import akka.actor.ActorIdentity
+import akka.actor.ReceiveTimeout
 
 abstract class Worker(path: ActorPath) extends Actor with ActorLogging {
-  val master = context.actorSelection(path)
+
+  import context._
+  import ExecutionContext.Implicits.global
+
+  /** send identification request */
+  def identify() = {
+    setReceiveTimeout(3 seconds)
+    actorSelection(path) ! Identify(path)
+  }
 
   /** will be implemented by concrete worker */
   @throws(classOf[Exception])
   def doWork(owner: ActorRef, work: Any, p: Promise[Unit])
 
-  import context._
-  import ExecutionContext.Implicits.global
+  def booting: Receive = {
+    case ActorIdentity(path, Some(master)) => {
+      setReceiveTimeout(Duration.Undefined)
+      become(idle(master))
+
+      // register this worker with master
+      master ! WorkerCreated(self)
+    }
+    case ActorIdentity(path, None) => log.error("Could not look up master actor.")
+    case ReceiveTimeout => identify()
+    case _ => log.error("I am receiving messages, but I am not connected to the master yet!")
+  }
 
   // worker's idle routine
-  def idle: Receive = {
+  def idle(master: ActorRef): Receive = {
 
     // if work is available request it
     case WorkIsReady =>
@@ -48,7 +70,7 @@ abstract class Worker(path: ActorPath) extends Actor with ActorLogging {
       doWork(owner, work, p)
 
       // change state to 'busy'
-      become(busy)
+      become(busy(master))
     }
 
     // master replied: no work available
@@ -59,7 +81,7 @@ abstract class Worker(path: ActorPath) extends Actor with ActorLogging {
   }
 
   // worker's busy routine
-  def busy: Receive = {
+  def busy(master: ActorRef): Receive = {
 
     // if already busy, don't handle any incoming work
     case WorkToBeDone(owner, work) => log.error("I am already working. I should not get any more items! {}", work)
@@ -68,7 +90,7 @@ abstract class Worker(path: ActorPath) extends Actor with ActorLogging {
     case DoneWorking => {
       log.info("Work complete!")
       // become idle
-      become(idle)
+      become(idle(master))
       // ... notify master 
       master ! WorkIsDone(self)
       // ... request more work
@@ -80,18 +102,14 @@ abstract class Worker(path: ActorPath) extends Actor with ActorLogging {
   }
 
   // start with being idle
-  def receive = idle
+  def receive = booting
 
   // we need to inform the master that we're up and running
   override def preStart = {
     super.preStart
-    master ! WorkerCreated(self)
-  }
 
-  // in case of a restart while working, re-issue work-request
-  override def preRestart(t: Throwable, msg: Option[Any]) {
-    msg map { case WorkToBeDone(owner, work) => master.tell(work, owner) }
-    super.preRestart(t, msg)
+    // send identification request
+    identify()
   }
 }
 
